072f79bf68669f36a09c7b12607a40e37a065835
Eugene <tech@phantaszm.com>
2020-08-25 12:18:31 +0800

    Added fish aliases

---
+++ .config/fish/functions/jit.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function jit --wraps='git --git-dir=$HOME/src/dotfiles/ --work-tree=$HOME' --description 'alias jit git --git-dir=$HOME/src/dotfiles/ --work-tree=$HOME'
+  git --git-dir=$HOME/src/dotfiles/ --work-tree=$HOME $argv;
+end
