a1f65381b04e6efd42671bb3d0d5993c139594fa
Eugene <tech@phantaszm.com>
2020-08-27 08:30:08 +0800

    Added install script

---
+++ .bin/jit-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone --bare git@github.com:phantaszm/dotfiles.git "$HOME"/.dotfiles
+else
+  echo "dotfiles repo already exists"
+fi
+function jit {
+  /usr/bin/git --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
+}
+# This backup logic isn't very good
+#if ! jit checkout; then
+#  echo "Checked out config.";
+#else
+#  mkdir -p .dotfiles-backup
+#  echo "Backing up pre-existing dot files.";
+#  jit checkout 2>&1 | grep -E "\s+\." | awk "{'print $2'}" | xargs -I{} mv {} .dotfiles-backup/{}
+#  jit checkout
+#fi;
+jit checkout
+jit config status.showUntrackedFiles no
+jit submodule update --init --recursive
--- .config/fish/functions/jit.fish
+++ .config/fish/functions/jit.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function jit --wraps='git --git-dir=$HOME/src/dotfiles/ --work-tree=$HOME' --description 'alias jit git --git-dir=$HOME/src/dotfiles/ --work-tree=$HOME'
-  git --git-dir=$HOME/src/dotfiles/ --work-tree=$HOME $argv;
+function jit --wraps='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' --description 'alias jit git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv;
 end
