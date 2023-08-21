c9b5907dd5fcec53783c593dfcad31449ef876d2
Eugene <tech@phantaszm.com>
2023-08-21 14:36:51 +0800

    More abbreviations

--- .config/fish/conf.d/git-shorts.fish
+++ .config/fish/conf.d/git-shorts.fish
@@ -4,6 +4,11 @@ abbr -a -g gdf  git diff
 abbr -a -g gbr  git branch -a
 abbr -a -g gco  git checkout
 abbr -a -g gcod git checkout develop
-abbr -a -g gcom git checkout master
+abbr -a -g gcom git checkout main
 abbr -a -g gcor git checkout release/sprint
 abbr -a -g ggpg set -x GPG_TTY (tty)
+
+abbr -a -g gffs git flow feature start
+abbr -a -g gfff git flow feature finish #(git branch --show-current | awk -F'/' '{print $2}')
+abbr -a -g gfhs git flow hotfix start
+abbr -a -g gfhf git flow hotfix finish #(git branch --show-current | awk -F'/' '{print $2}')
