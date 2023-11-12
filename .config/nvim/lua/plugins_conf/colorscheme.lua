ab4ade09eabd6b000a219b9897500d83844fc4ec
Eugene <tech@phantaszm.com>
2023-11-12 22:40:50 +0800

    Change default colorscheme

--- .config/nvim/lua/plugins_conf/colorscheme.lua
+++ .config/nvim/lua/plugins_conf/colorscheme.lua
@@ -1,1 +1,5 @@
-vim.cmd('colorscheme base16-tomorrow-night')
+vim.opt.background = "dark"
+-- vim.cmd('colorscheme base16-tomorrow-night')
+-- vim.cmd([[colorscheme base16-tomorrow-night]])
+vim.cmd([[colorscheme gruvbox]])
+
